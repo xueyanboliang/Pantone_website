8393314a9a8f2162bd2bcdf8ef6d6c975314fb4a
ubuntu <18751803026@163.com>
2018-02-26 21:06:01 +0800

    项目初始化

---
+++ src/index.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import ReactDOM from 'react-dom'
+import { Provider} from 'react-redux'
+import store from './redux/store/configureStore'
+import App from './App'
+import registerServiceWorker from './registerServiceWorker'
+import './style/style.css'
+
+ReactDOM.render(
+    <Provider store={store}>
+        <App />
+    </Provider>
+, document.getElementById("root"))
+registerServiceWorker()
